af20a8699ff088dcb5a571218e0bfffdb35a7b04
Calum Bird <calum@trelent.net>
2023-07-20 20:03:59 -0700

    Conf checks key in setup function

--- gptask/conf.py
+++ gptask/conf.py
@@ -1,6 +1,7 @@
+import click
 import os
 
-def ensure_dir():
+def setup():
     base_dir = os.path.expanduser('~/.gptask')
     if not os.path.exists(base_dir):
         os.makedirs(base_dir)
@@ -8,6 +9,19 @@ def ensure_dir():
     prompts_dir = os.path.expanduser('~/.gptask/prompts')
     if not os.path.exists(prompts_dir):
         os.makedirs(prompts_dir)
+        
+    # Check for key
+    key_path = os.path.expanduser('~/.gptask/openai.key')
+    if not os.path.exists(key_path):
+        # Check if key is in env
+        if 'OPENAI_API_KEY' in os.environ and click.confirm('Found OpenAI API key in environment. Would you like to save it to ~/.gptask/openai.key? (Y/n)'):
+            with open(key_path, 'w') as f:
+                f.write(os.environ['OPENAI_API_KEY'])
+        else:
+            # Ask user to enter key
+            key = click.prompt('Enter your OpenAI API key.', type=str)
+            with open(key_path, 'w') as f:
+                f.write(key)
 
 def load_prompts():
     prompts = {}
@@ -21,9 +35,6 @@ def load_key():
     # load from ~/.gptask/openai.key
     with open(os.path.expanduser('~/.gptask/openai.key')) as f:
         key = f.read().strip()
-        
-    if len(key) == 0:
-        key = os.environ['OPENAI_API_KEY']
         
     if len(key) == 0:
         raise Exception('No OpenAI key found')
