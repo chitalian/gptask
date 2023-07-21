1d90f4c92b0f2328dd4b299e012e94394bed7a0c
Justin <justintorre75@gmail.com>
2023-07-20 19:38:38 -0700

    move to gptask file

--- gptask/gptask.py
+++ gptask/gptask.py
@@ -1,5 +1,13 @@
-def main():
-    print("Hi")
+import click
 
-if __name__ == "__main__":
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def main(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+if __name__ == '__main__':
     main()
