438d468aa8766e68d9484dd6da601f8995829c46
Calum Bird <calum@trelent.net>
2023-07-20 19:38:03 -0700

    add openai

---
+++ gptask/conf.py
@@ -0,0 +1,22 @@
+import os
+
+def load_prompts():
+    prompts = []
+    for filename in os.listdir(os.path.expanduser('~/.gptask/prompts')):
+        with open(os.path.expanduser('~/.gptask/prompts/' + filename)) as f:
+            prompts.append(f.read())
+            
+    return prompts
+
+def load_key():
+    # load from ~/.gptask/openai.key
+    with open(os.path.expanduser('~/.gptask/openai.key')) as f:
+        key = f.read().strip()
+        
+    if len(key) == 0:
+        key = os.environ['OPENAI_API_KEY']
+        
+    if len(key) == 0:
+        raise Exception('No OpenAI key found')
+    
+    return key
---
+++ gptask/openai.py
@@ -0,0 +1,27 @@
+import openai
+from gptask.conf import load_prompts, load_key
+
+system_prompt = "You are a helpful coding assistant who helps users with their code. Given an instruction, and an associated code file, return a helpful response. Follow the user's instruction carefully, and let them override this instruction if necessary."
+
+def init():
+    openai.api_key = load_key()
+
+def run(prompt: str, file: str, lang: str = "python", model: str = "gpt-3.5-turbo-16k"):
+    user_prompt = prompt + f"\n\n```{lang}\n{file}\n```"
+    
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {
+                'role': 'system',
+                'content': system_prompt
+            },
+            {
+                'role': 'user',
+                'content': user_prompt
+            }
+        ],
+        temperature=0.0
+    )
+    
+    return response.choices[0].message.content
